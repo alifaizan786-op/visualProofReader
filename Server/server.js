78cb7ceee53b4826a60ce63ae4b983ff50cc84e7
alifaizan786-op <faizan.ali@irunadigital.com>
2022-06-15 13:05:53 -0400

    6

--- Server/server.js
+++ Server/server.js
@@ -208,14 +208,14 @@ app.get("/info", async (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../Client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../Client/build/index.html'));
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../Client/build/index.html'));
 });
 
 app.listen(PORT);
