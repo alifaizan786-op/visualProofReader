10810ddacd51bbcc06d613ad16621c4c2746015b
alifaizan786-op <faizan.ali@irunadigital.com>
2022-06-15 12:31:27 -0400

    tryna push to heroku 1

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,9 @@ const app = express();
 
 const PORT = process.env.PORT || 4000
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 require("events").EventEmitter.prototype._maxListeners = 100;
 
